a9c8719c190e19e799958b512414f4b4da7991b4
omaralnajjar-code <omaralnajjar953@gmail.com>
2020-05-03 21:27:46 +0300

    Added guest name, change background & text  colors

---
+++ app/src/main/java/com/example/connectfour/GuestName.kt
@@ -0,0 +1,16 @@
+package com.example.connectfour
+
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+
+class GuestName : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_name)
+    }
+
+}
